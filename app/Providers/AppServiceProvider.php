b8735d6bb35f421a4ff63d365df33d8d67380f35
Victor Lopez <victor.98.javier@gmail.com>
2023-11-20 07:11:55 -0500

    fix: Prevent N + 1 error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Database\Eloquent\Model;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Disable lazy loading in development mode
+        Model::preventLazyLoading(! app()->isProduction());
+
     }
 }
