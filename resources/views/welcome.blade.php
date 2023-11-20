ac72d1e140573ad6847499d24486077832dcebc4
Victor Lopez <victor.98.javier@gmail.com>
2023-11-20 11:00:38 -0500

    feat: Update welcome page
    Update view of the restaurants on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,9 @@
-<x-site-layout title="Welcome" >
+<x-site-layout title="Welcome">
 
     <h2 class="font-bold">All Restaurants </h2>
     <div class="grid grid-cols-3 gap-4">
         @foreach($restaurants as $restaurant)
-            <div>
-                {{$restaurant->name }}
-            </div>
+        <x-simple-card imageUrl="https://via.placeholder.com/640x480.png/00ff77?text=No+Image" :title="$restaurant->name" subtitle='' route='welcome' />
         @endforeach
     </div>
 </x-site-layout>
