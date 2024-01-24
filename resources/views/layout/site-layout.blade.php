4b866e6b81b0587ed00262bb9eec1f453906691f
Victor Lopez <victor.98.javier@gmail.com>
2024-01-24 17:47:08 +0100

    Change App name

--- resources/views/layout/site-layout.blade.php
+++ resources/views/layout/site-layout.blade.php
@@ -4,9 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>HTML5 Boilerplate</title>
+    <title>Smart Table</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="//unpkg.com/alpinejs" defer></script>
+    @livewireStyles
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body class="h-full">
